d0f4bcedf8a9cf29bc2d1f333abd052f720d12cb
Ivan Chee <xenzaki@msn.com>
2017-09-07 17:04:33 +1000

    Create LoginActivity, CreateAccountActivity, DashboardActivity

--- CatchMeIfYouCan/app/src/main/java/com/comp30022/tarth/catchmeifyoucan/MainActivity.java
+++ CatchMeIfYouCan/app/src/main/java/com/comp30022/tarth/catchmeifyoucan/MainActivity.java
@@ -1,13 +1,65 @@
 package com.comp30022.tarth.catchmeifyoucan;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button buttonCreateAccount;
+    Button buttonLogin;
+    Button buttonExit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonCreateAccount = (Button) findViewById(R.id.buttonCreateAccount);
+        buttonCreateAccount.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createAccount(v);
+            }
+        });
+
+        buttonLogin = (Button) findViewById(R.id.buttonLogin);
+        buttonLogin.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login(v);
+            }
+        });
+
+        buttonExit = (Button) findViewById(R.id.buttonExit);
+        buttonExit.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                exit(v);
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        finish();
+        System.exit(0);
+    }
+
+    public void createAccount(View view) {
+        Intent intent = new Intent(this, CreateAccountActivity.class);
+        startActivity(intent);
+    }
+
+    public void login(View view) {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+    public void exit(View view) {
+        finish();
+        System.exit(0);
     }
 }
