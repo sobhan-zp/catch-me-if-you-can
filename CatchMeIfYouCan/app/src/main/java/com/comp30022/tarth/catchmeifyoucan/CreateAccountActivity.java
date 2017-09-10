e4925c6f5a351805dd99481454c3e40c2afa2875
Ivan Chee <xenzaki@msn.com>
2017-09-10 16:53:26 +1000

    U: Client, CreateAccountActivity

--- CatchMeIfYouCan/app/src/main/java/com/comp30022/tarth/catchmeifyoucan/CreateAccountActivity.java
+++ CatchMeIfYouCan/app/src/main/java/com/comp30022/tarth/catchmeifyoucan/CreateAccountActivity.java
@@ -1,6 +1,7 @@
 package com.comp30022.tarth.catchmeifyoucan;
 
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -9,29 +10,41 @@ public class CreateAccountActivity extends AppCompatActivity {
 
     Button buttonCreate;
     Button buttonBack;
+    ConnectTask mConnectTask;
 
-    // TCP Client
-    Client mClient;
+    // Example message sent to the server
+    String mMessage = "{\"username\": \"vikramgk\", \"password\": \"cellotape\", \"client_ip\": 1234, \"email\": \"nigerian_prince@student.unimelb.edu.au\", \"name\": \"Nigerian Price\", \"date_of_birth\": 0}";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
 
+        if (android.os.Build.VERSION.SDK_INT > 9) {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }
+
         buttonCreate = (Button) findViewById(R.id.buttonCreate);
         buttonCreate.setOnClickListener(new Button.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // Starts a connection to the server
-                new ConnectTask().execute("");
+                mConnectTask = new ConnectTask();
+                mConnectTask.execute("");
+
+                //mConnectTask.doInBackground(mMessage);
+
+                /*
                 // Sends a message to the server
                 if (mClient != null) {
-                    mClient.sendMessage("Testing");
+                    mClient.sendMessage(mMessage);
                 }
                 // Stopping the connection to the server
                 if (mClient != null) {
                     mClient.close();
                 }
+                */
             }
         });
 
