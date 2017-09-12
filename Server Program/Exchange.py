8fa10a080e8b972c72feb68a23d98ae59578202e
zirenxiao <zirenx@student.unimelb.edu.au>
2017-09-12 23:34:21 +1000

    Update Exchange.py

--- Server Program/Exchange.py
+++ Server Program/Exchange.py
@@ -1,5 +1,6 @@
 import json
 import uuid
+import ast
 import socket, select
 from Account import Account
 from Mysql import Mysql
@@ -11,8 +12,9 @@ RECOVER_PASSWORD = 102
 class Exchange:
 
     def receive(self, msg):
-        format_msg = json.dumps(msg)
-        return json.loads(format_msg)
+        ##format_msg = json.dumps(msg[0:len(msg)-1])
+        ##return json.loads(msg[0:len(msg)-1])
+        return json.loads(msg)
 
     def send(self, dict_msg, sock):
         format_msg = json.dumps(dict_msg)
@@ -21,15 +23,22 @@ class Exchange:
     """ Parses the received message and performs corresponding actions """
     def process(self, acc, db, sock, data):
         ## Will finish other actions once we update the JSON format
-        print("A: Register")
-        msg = receive(data)
-        print("R:" + msg)
-        if msg["action"] == REGISTER:
-            print("A: Register")
-            self.register(acc, db, sock, msg)
-        if msg["action"] == LOGIN:
-            print("A: Login")
-            self.login(acc, msg)
+        msg = self.receive(data)
+        print(msg)
+        if (type(msg)==dict):
+            if ("action" in msg.keys()):
+                if msg["action"] == REGISTER:
+                    print("A: Register")
+                    self.register(acc, db, sock, msg)
+                elif msg["action"] == LOGIN:
+                    print("A: Login")
+                    self.login(acc, msg)
+                else:
+                    print("E: Wrong Action Code")
+            else:
+                print("E: No Action")
+        else:
+            print("E: Wrong Message")
 
     """ Register action """
     def register(self, acc, db, sock, msg):
