6656ddc91e256473a5250034d8b1ca0a09b81430
zirenxiao <zirenx@student.unimelb.edu.au>
2017-09-16 01:56:09 +1000

    INIT

--- Server Program/Node.JS Web Socket Connection/ht/server.js
+++ Server Program/Node.JS Web Socket Connection/ht/server.js
@@ -1,22 +1,28 @@
-var WebSocket = require('ws');
-var WebSocketServer = WebSocket.Server,
-wss = new WebSocketServer({ port: 80 });
-var uuid = require('node-uuid');
-var clients = [];
-var clientIndex = 1;
+// Constant
+var SERVER_PORT = 80
 
-function wsSendTo(from_uuid, to_uuid, message){
+// Requires
+var WebSocket = require('ws');
+var uuid = require('node-uuid');
+
+// Variables
+var WebSocketServer = WebSocket.Server,
+    wss = new WebSocketServer({ port: SERVER_PORT });
+var clients = [];
+
+console.log('Server Starts at port %d', SERVER_PORT);
+
+function ws_send_to(from_uuid, to_uuid, message){
 
 }
 
-function wsSend(type, client_uuid, nickname, message) {
+function ws_send_all(type, client_uuid, message) {
     for (var i = 0; i < clients.length; i++) {
         var clientSocket = clients[i].ws;
         if (clientSocket.readyState === WebSocket.OPEN) {
             clientSocket.send(JSON.stringify({
                 "type": type,
                 "id": client_uuid,
-                "nickname": nickname,
                 "message": message
             }));
         }
@@ -26,39 +32,20 @@ function wsSend(type, client_uuid, nickname, message) {
 // WebSocket open && stay connecting action
 wss.on('connection', function(ws) {
     var client_uuid = uuid.v4();
-    var nickname = "AnonymousUser" + clientIndex;
-    clientIndex += 1;
-    clients.push({ "id": client_uuid, "ws": ws, "nickname": nickname });
+    clients.push({ "id": client_uuid, "ws": ws});
     console.log('client [%s] connected', client_uuid);
-    var connect_message = nickname + " has connected";
-    wsSend("notification", client_uuid, nickname, connect_message);
 
     // WebSocket received message action
     ws.on('message', function(message) {
-        if (message.indexOf('/nick') === 0) {
-            var nickname_array = message.split(' ');
-            if (nickname_array.length >= 2) {
-                var old_nickname = nickname;
-                nickname = nickname_array[1];
-                var nickname_message = "Client " + old_nickname + " changed to " + nickname;
-                wsSend("nick_update", client_uuid, nickname, nickname_message);
-            }
-        } else {
-            console.log('client [%s] message [%s]', client_uuid, message);
-            wsSend("message", client_uuid, nickname, message);
-        }
+        console.log('client [%s] message [%s]', client_uuid, message);
     });
 
+    // A client disconnected
+    // Delete its ID from client array
     var closeSocket = function(customMessage) {
         for (var i = 0; i < clients.length; i++) {
             if (clients[i].id == client_uuid) {
-                var disconnect_message;
-                if (customMessage) {
-                    disconnect_message = customMessage;
-                } else {
-                    disconnect_message = nickname + " has disconnected";
-                }
-                wsSend("notification", client_uuid, nickname, disconnect_message);
+                console.log('client [%s] disconnected', client_uuid);
                 clients.splice(i, 1);
             }
         }
@@ -71,7 +58,7 @@ wss.on('connection', function(ws) {
 
     // Server close action
     process.on('SIGINT', function () {
-        console.log("Closing things");
+        console.log("Server Closing.....");
         closeSocket('Server has disconnected');
         process.exit();
     });
