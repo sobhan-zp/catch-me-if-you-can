6258277f12c8be817fec1ba46ab42c384a9cc540
zirenxiao <zirenx@student.unimelb.edu.au>
2017-09-16 22:22:45 +1000

    register && login action finished

--- Server Program/Node.JS Web Socket Connection/README.md
+++ Server Program/Node.JS Web Socket Connection/README.md
@@ -2,12 +2,12 @@ WebSocket Server
 
 Requirements:
 
-    1. node version 6.11.3 or above
-    
+    1. node version 8.5.0 or above
+
     2. npm version 3.10.10 or above
-    
+
     3. CentOS 5/Windows 2003 or above
-    
+
     4. Port 80 accessible
 
 To install Node.JS
@@ -17,28 +17,27 @@ To install Node.JS
 To install MariaDB
 
     - for CentOS 7
-    
+
         - Enter following command to install
-        
+
             yum install mariadb mariadb-server -y
-            
+
         - Start MariaDB
-        
+
             systemctl start Mariadb
-            
+
         - Start on boot
-        
+
             systemctl enable mariadb
-            
+
         - MariaDB initiallize
-        
+
             mysql_secure_installation
 
 To run the server
 
     - for Linux && Windows
-    
+
         - cd /%YOUR PATH%//ht
-        
+
         - sudo node index.js
-        
--- Server Program/Node.JS Web Socket Connection/ht/server.js
+++ Server Program/Node.JS Web Socket Connection/ht/server.js
@@ -1,5 +1,14 @@
 // Constant
-var SERVER_PORT = 80
+DEFAULT_USER_LEVEL = 0;
+SERVER_PORT = 80;
+REGISTER_ACTION = 100;
+LOGIN_ACTION = 101;
+MESSAGE_ACTION = 102;
+LOGIN_SUCCESS_CODE = 200;
+LOGIN_USER_NON_EXIST_CODE = 201;
+LOGIN_EXIST_CODE = 202;
+RESISTER_SUCCESS = 300;
+REGISTER_FAIL = 301;
 
 // Requires
 var WebSocket = require('ws');
@@ -13,33 +22,134 @@ var con = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "ht!!!",
-    database: "housetarth"
+    database: "house_tarth"
 });
 var clients = [];
 
 console.log('Server Starts At Port %d', SERVER_PORT);
+set_database_con();
 
-con.connect(function(err) {
-    if (err) {
-        console.log('Database Connection Failed');
-        console.log('%s', err);
-        process.exit();
-    }
-    console.log("Database Connected");
-
-});
-
-con.query("SELECT * FROM test", function (err, result, fields) {
-    if (err) throw err;
-    console.log("Result:%s",JSON.stringify(result));
-});
-
-
-function ws_send_to(from_uuid, to_uuid, message){
-
+function set_database_con(){
+    con.connect(function(err) {
+        if (err) {
+            console.log('Database Connection Failed');
+            console.log('%s', err);
+            process.exit();
+        }
+        console.log("Database Connected");
+    });
 }
 
-function ws_send_all(type, client_uuid, message) {
+
+function login_check(username, password, sock, client_uuid){
+    var fails = false;
+    var code = LOGIN_SUCCESS_CODE;
+    for (var i = 0; i < clients.length; i++) {
+        if (clients[i].username === username){
+            fails = true;
+            code = LOGIN_EXIST_CODE;
+            break;
+        }
+    }
+    if (!fails){
+        var sql = "SELECT * FROM account where username = '" + username + "' and password = '" + password + "'";
+        con.query(sql, function (err, result, rows, fields) {
+            if (err) throw err;
+            if (result.length>0){
+                var temp_info = {
+                    "id": client_uuid,
+                    "ws": sock,
+                    "username": result[0]['username'],
+                    "name": result[0]['name'],
+                    "email": result[0]['email'],
+                    "db_id": result[0]['id'],
+                    "lv": result[0]['user_level']
+                };
+                var feedback = {
+                    "status": "success",
+                    "code": code
+                };
+                clients.push(temp_info);
+                sys_send_to_id(client_uuid, JSON.stringify(feedback));
+            }else{
+                fails = true;
+                code = LOGIN_USER_NON_EXIST_CODE;
+            }
+        });
+    }
+    if (fails){
+        var feedback = {
+            "status": "failed",
+            "code": code
+        };
+        sys_send_to_id(client_uuid, JSON.stringify(feedback));
+    }
+}
+
+
+function register(username, password, email, name, sock){
+    var sql = "INSERT INTO account (username, password, email, name, user_level) VALUES ('" + username + "','" + password +"','" + email + "','" + name + "','" + DEFAULT_USER_LEVEL + "')";
+    con.query(sql, function (err, result) {
+        var feedback;
+        if (err){
+            feedback = {
+                "status": "failed",
+                "code": REGISTER_FAIL
+            };
+        }else{
+            feedback = {
+                "status": "success",
+                "code": RESISTER_SUCCESS
+            };
+        }
+        sys_send_to_sock(sock, JSON.stringify(feedback));
+    });
+}
+
+function sys_send_to_id(id, message){
+    for (var i = 0; i < clients.length; i++) {
+        if (clients[i].id = id){
+            var clientSocket = clients[i].ws;
+            if (clientSocket.readyState === WebSocket.OPEN) {
+                clientSocket.send(JSON.stringify({
+                    "type": "notification",
+                    "id": id,
+                    "message": message
+                }));
+            }
+            console.log('A system message has been sent');
+            break;
+        }
+    }
+}
+
+function sys_send_to_sock(sock, message){
+        if (sock.readyState === WebSocket.OPEN) {
+            sock.send(JSON.stringify({
+                "type": "notification",
+                "message": message
+            }));
+        }
+        console.log('A system message has been sent');
+}
+
+function user_send_to_id(from_id, to_id, message){
+    for (var i = 0; i < clients.length; i++) {
+        if (clients[i].id = to_id){
+            var clientSocket = clients[i].ws;
+            if (clientSocket.readyState === WebSocket.OPEN) {
+                clientSocket.send(JSON.stringify({
+                    "type": "notification",
+                    "from": from_id,
+                    "message": message
+                }));
+            }
+            break;
+        }
+    }
+}
+
+function msg_send_all(type, client_uuid, message) {
     for (var i = 0; i < clients.length; i++) {
         var clientSocket = clients[i].ws;
         if (clientSocket.readyState === WebSocket.OPEN) {
@@ -55,17 +165,25 @@ function ws_send_all(type, client_uuid, message) {
 // WebSocket open && stay connecting action
 wss.on('connection', function(ws) {
     var client_uuid = uuid.v4();
-    clients.push({ "id": client_uuid, "ws": ws});
+    //clients.push({ "id": client_uuid, "ws": ws});
     console.log('client [%s] connected', client_uuid);
 
     // WebSocket received message action
     ws.on('message', function(message) {
+        var data = JSON.parse(message);
         console.log('client [%s] message [%s]', client_uuid, message);
+        if (data.action == LOGIN_ACTION){
+            login_check(data.username, data.password, ws, client_uuid);
+        }else if (data.action == REGISTER_ACTION){
+            register(data.username, data.password, data.email, data.name, ws);
+        }else if (data.action == MESSAGE_ACTION){
+            register(data.username, data.password, data.email, data.name, ws);
+        }
     });
 
     // A client disconnected
     // Delete its ID from client array
-    var closeSocket = function(customMessage) {
+    var closeSocket = function() {
         for (var i = 0; i < clients.length; i++) {
             if (clients[i].id == client_uuid) {
                 console.log('client [%s] disconnected', client_uuid);
