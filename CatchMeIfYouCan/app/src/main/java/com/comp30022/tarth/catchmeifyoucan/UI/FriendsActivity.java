6e5e31a88c824df1b9619bd5b3401745a0b7ac4b
Vikram Gopalan-Krishnan <Vikram Gopalan-Krishnan>
2017-09-21 14:23:36 +1000

    Friends List UI

---
+++ CatchMeIfYouCan/app/src/main/java/com/comp30022/tarth/catchmeifyoucan/UI/FriendsActivity.java
@@ -0,0 +1,28 @@
+package com.comp30022.tarth.catchmeifyoucan.UI;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.comp30022.tarth.catchmeifyoucan.R;
+
+public class FriendsActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_friends);
+
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        finish();
+    }
+
+
+}
