6a21aaec403b2e05a01ea17b1d5d42e01be45b0a
Ivan Chee <xenzaki@msn.com>
2017-10-23 03:24:47 +1100

    Added README; Created Activity test suite

--- CatchMeIfYouCan/app/src/androidTest/java/com/comp30022/tarth/catchmeifyoucan/ApplicationInstrumentedTest.java
+++ CatchMeIfYouCan/app/src/androidTest/java/com/comp30022/tarth/catchmeifyoucan/ApplicationInstrumentedTest.java
@@ -9,13 +9,9 @@ import org.junit.runner.RunWith;
 
 import static org.junit.Assert.*;
 
-/**
- * Instrumentation test, which will execute on an Android device.
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
     @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
@@ -23,4 +19,5 @@ public class ExampleInstrumentedTest {
 
         assertEquals("com.comp30022.tarth.catchmeifyoucan", appContext.getPackageName());
     }
+
 }
